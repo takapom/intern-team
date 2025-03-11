1dc1fdd2fcb5f0cc12a29cfcb765b579611b4368
Gitudon <udon.oniku.029@gmail.com>
2025-03-11 10:49:54 +0900

    feat: プレビューのプレースホルダー完成

--- vite-project/src/componets/Preview.jsx
+++ vite-project/src/componets/Preview.jsx
@@ -2,6 +2,10 @@ import { useContext, useState } from "react";
 import "../style/Preview.css";
 
 export default function Preview() {
+    // const { bukkenmei, yachin, gaikan, naikan, shosai } = useContext(PreviewContext);
+    // const [bukkenmei, setBukkenmei] = useState("");
+    // みたいな...
+    // 情報がない場合はプレースホルダーを表示しておく
     return (
         <div className="preview-main">
             <div className="preview-header">
@@ -23,7 +27,20 @@ export default function Preview() {
                 </div>
                 <div className="preview-shosai">
                     <div className="preview-info">
-                        <h2></h2>
+                        <div className="preview-madori">
+                            <h2>間取り</h2>
+                        </div>
+                        <div className="preview-otherinfo">
+                            <div className="preview-access_placeholder">
+                                <h2>アクセス</h2>
+                            </div>
+                            <div className="preview-joken_placeholder">
+                                <h2>条件</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="preview-bukken_info_placeholder">
+                        <h2>物件情報</h2>
                     </div>
                 </div>
             </div>
