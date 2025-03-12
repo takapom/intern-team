bdbb3eb86bca4ea25a476f6508670e32f408a69e
Intern01 <intern01@inm121noMacBook-Pro.local>
2025-03-12 10:48:04 +0900

    情報入力欄とプレビューの配置修正と値の受け渡し

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import InputItem from './components/Input.jsx'
 import Information from "./components/information"
 import Preview from "./components/preview"
 import './App.css';
 import { createContext, useState } from "react";
 
 export const InfomationContext = createContext([
-    { name : "" , price : 0 }, () => {}
+    { name : "" , rent : 0 , area: "", address: "", station: "", published: "" }, () => {}
 ]);
-
 
 function App() {
   const [value, setValue] = useState({
     name : "",
-    price : 0
+    rent : 0,
+    area: "",
+    address: "", 
+    station: "", 
+    published: ""
   });
 
   return (
+    // <div className='information-preview'>
+    // <div className='preview'>
+    //   <InfomationContext.Provider value={[value, setValue]}>
+    //   <Information />
+    //   <Preview />
+    // </InfomationContext.Provider>
+    // </div>
+    // </div>
+    <InfomationContext.Provider value={[value, setValue]}>
     <div className='information-preview'>
-    <div className='information'>
-      <Information />
+      <div className='information'>
+          <Information />
       </div>
-    <div className='preview'>
-      <InfomationContext.Provider value={[value, setValue]}>
-    </InfomationContext.Provider>
-    <Preview />
+      <div className='preview'>
+        <Preview />
+      </div>
     </div>
-    </div>
+  </InfomationContext.Provider>
   );
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { InfomationContext } from '../App.jsx';
+import Preview from './preview.jsx';
+// import { create } from '@mui/material/styles/createTransitions.js';
 
 const InputItem = () => {
   console.log('aaaa')
@@ -12,15 +14,52 @@ const InputItem = () => {
     published: '',
   });
 
+  const [value, setValue] = useContext(InfomationContext);
+
   const InputChange = (e) => {
-    const { id, value } = e.target;
+    const { id, value: inputVal } = e.target;
     setInputValue((prev) => ({
       ...prev,
-      [id]: value,
+      [id]: inputVal,
     }));
+    if (id === 'name') {
+      setValue((prevValue) => ({
+        ...prevValue,
+        name: inputVal,
+      }));
+    } else if (id === 'rent') {
+      setValue((prevValue) => ({
+        ...prevValue,
+        price: Number(inputVal) || 0,
+      }));
+    }
+    else if (id === 'area') {
+      setValue((prevValue) => ({
+        ...prevValue,
+        area: Number(inputVal) || 0,
+      }));
+    }
+    else if (id === 'address') {
+      setValue((prevValue) => ({
+        ...prevValue,
+        address: Number(inputVal) || 0,
+      }));
+    }
+    else if (id === 'station') {
+      setValue((prevValue) => ({
+        ...prevValue,
+        station: Number(inputVal) || 0,
+      }));
+    }
+    else if (id === 'published') {
+      setValue((prevValue) => ({
+        ...prevValue,
+        published: Number(inputVal) || 0,
+      }));
+    }
   };
 
-  const [value, setValue] = useContext(InfomationContext);
+
 
   return (
     <div
@@ -38,6 +77,7 @@ const InputItem = () => {
         物件名
       </label>
       <input
+        placeholder='例'
         type="text"
         id="name"
         value={inputValue.name}
@@ -59,6 +99,7 @@ const InputItem = () => {
         家賃
       </label>
       <input
+      placeholder='例'
         type="text"
         id="rent"
         value={inputValue.rent}
@@ -80,6 +121,7 @@ const InputItem = () => {
         面積
       </label>
       <input
+        placeholder='例'
         type="text"
         id="area"
         value={inputValue.area}
@@ -102,6 +144,7 @@ const InputItem = () => {
         所在地
       </label>
       <input
+        placeholder='例'
         type="text"
         id="address"
         value={inputValue.address}
@@ -124,6 +167,7 @@ const InputItem = () => {
         最寄駅
       </label>
       <input
+        placeholder='例'
         type="text"
         id="station"
         value={inputValue.station}
@@ -145,6 +189,7 @@ const InputItem = () => {
         公開日
       </label>
       <input
+        placeholder='例'
         type="text"
         id="published"
         value={inputValue.published}
