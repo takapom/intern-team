a8d6d8425b63e18b7650f8cdab1c6179cdaa0439
Gitudon <udon.oniku.029@gmail.com>
2025-03-13 15:03:41 +0900

    feat: ボタンのデザイン

--- src/components/PrintPDF.jsx
+++ src/components/PrintPDF.jsx
@@ -8,23 +8,23 @@ export default function PrintPdf() {
         const target = document.getElementById('pdf');
         if (target === null) return;
         html2canvas(target, { scale: 2.5 }).then((canvas) => {
-        const imgData = canvas.toDataURL('image/svg', 1.0);
-        const pdf = new jsPDF('l', 'mm', 'a4');
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = pdf.internal.pageSize.getHeight();
-        const imgWidth = canvas.width / 10;
-        const imgHeight = canvas.height / 10;
-        const x = (pdfWidth - imgWidth) / 2;
-        const y = (pdfHeight - imgHeight) / 2;
-        pdf.addImage(imgData, 'SVG', x, y, imgWidth, imgHeight);
-        const date = new Date();
-        const dateString = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}_${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
-        pdf.save(`preview-${dateString}.pdf`);
+            const imgData = canvas.toDataURL('image/svg', 1.0);
+            const pdf = new jsPDF('l', 'mm', 'a4');
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+            const imgWidth = canvas.width / 10;
+            const imgHeight = canvas.height / 10;
+            const x = (pdfWidth - imgWidth) / 2;
+            const y = (pdfHeight - imgHeight) / 2;
+            pdf.addImage(imgData, 'SVG', x, y, imgWidth, imgHeight);
+            const date = new Date();
+            const dateString = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}_${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+            pdf.save(`preview-${dateString}.pdf`);
         });
     };
     return (
     <>
-        <button type = 'button' onClick={printDocument} className='c-btn'>
+        <button type = 'button' onClick={printDocument} className='dl-btn'>
             PDFダウンロード
         </button>
     </>
