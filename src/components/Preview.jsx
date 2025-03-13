fa58ab4dce077c866d6ddfc82b6adee5cc6d0bc7
Gitudon <udon.oniku.029@gmail.com>
2025-03-13 17:19:30 +0900

    feat: プレビュー画面の成型、Contextの初期値の変更

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,27 +4,19 @@ import { InformationContext } from "../App";
 import { formatNumber } from "../utils/formatnumber";
 
 export default function Preview() {
-    const [value] = useContext(InformationContext);
-    let NaikanImage = null;
-    if (value.exterior){
-        NaikanImage = URL.createObjectURL(value.exterior);
-    }
-    let GaikanImage = null;
-    if (value.interview){
-        GaikanImage = URL.createObjectURL(value.interview);
-    }
-    let MadoriImage = null;
-    if (value.floor){
-        MadoriImage = URL.createObjectURL(value.floor);
-    }
+    const [properyInfo] = useContext(InformationContext);
+    let NaikanImage, GaikanImage, MadoriImage;
+    properyInfo.exterior ? (NaikanImage = URL.createObjectURL(properyInfo.exterior)) : (NaikanImage = null);
+    properyInfo.interview ? GaikanImage = URL.createObjectURL(properyInfo.interview) : GaikanImage = null;
+    properyInfo.floor ? MadoriImage = URL.createObjectURL(properyInfo.floor) : MadoriImage = null;
     return (
         <div className="preview-main" id="pdf">
             <div className="preview-header">
                 <div className="preview-bukkenmei">
-                    {!value.name ? <h2>物件名</h2> : <p>{value.name}</p>}
+                    {!properyInfo.name ? <h2>物件名</h2> : <p>{properyInfo.name}</p>}
                 </div>
                 <div className="preview-yachin">
-                    {!value.rent ? <h2>家賃</h2> : <p>賃料: <b>{formatNumber(value.rent)}円</b></p>}
+                    {!properyInfo.rent ? <h2>家賃</h2> : <p>賃料: <b>{formatNumber(properyInfo.rent)}円</b></p>}
                 </div>
             </div>
             <div className="preview-body">
@@ -45,48 +37,58 @@ export default function Preview() {
                             {MadoriImage && <img src={MadoriImage} style={{ height: "25em", width: "19.75em" }} />}
                         </div>
                         <div className="preview-otherinfo">
-                            {
-                                !value.address ?
-                                <div className="preview-access_placeholder">
+                            {!properyInfo.address
+                                ? <div className="preview-access_placeholder">
                                     <h2>アクセス</h2>
                                 </div>
                                 : <div className="preview-access">
                                     <h3>所在地</h3>
-                                    <p>{value.address}</p>
-                                    {value.station && (
+                                    <p>{properyInfo.address}</p>
+                                    {properyInfo.station && (
                                         <>
                                             <h3>最寄駅</h3>
-                                            <p>{value.station}</p>
+                                            <p>{properyInfo.station}</p>
                                         </>
                                     )}
                                 </div>
                             }
-                            <div className="preview-joken_placeholder">
-                                <div className="preview-joken_placeholder">
-                                <p>敷金/礼金: <b>{formatNumber(value.deposit)}円</b></p>
-                                <p>更新料: <b>{formatNumber(value.new)}円</b></p>
-                                <p>契約期間:  {value.contract}</p>
-                                <p>駐車場:  {value.park}</p>
-                                <p>保険加入:  {value.insurance}</p>
+                            {!properyInfo.deposit
+                                ? <div className="preview-joken_placeholder">
+                                    <h2>契約条件</h2>
+                                    <h2>必須条件</h2>
                                 </div>
+                                : <div className="preview-joken">
+                                <p>敷金/礼金: <b>{formatNumber(properyInfo.deposit)}円</b></p>
+                                {properyInfo.new && (
+                                    <p>更新料: <b>{formatNumber(properyInfo.new)}円</b></p>
+                                )}
+                                {properyInfo.contract && (
+                                    <p>契約期間: <b>{properyInfo.contract}年</b></p>
+                                )}
+                                {properyInfo.park && (
+                                    <p>駐車場: <b>{properyInfo.park}</b></p>
+                                )}
+                                {properyInfo.insurance && (
+                                    <p>保険加入: <b>{properyInfo.insurance}</b></p>
+                                )}
+                                </div>
+                            }
+                        </div>
+                    </div>
+                    {!properyInfo.area
+                        ? <div className="preview-bukken_info_placeholder">
+                            <h2>物件情報</h2>
+                        </div>
+                        : <div className="preview-bukken_info">
+                            <div className="preview-bukken_info_left">
+                            <p>面積: <b>{properyInfo.area} ㎡</b></p>
+                            {properyInfo.published && (
+                                <p>公開日: <b>{properyInfo.published}</b></p>
+                            )}
+                            </div>
+                            <div className="preview-bukken_info_right">
                             </div>
                         </div>
-                    </div>
-                    {!value.area ?
-                    <div className="preview-bukken_info_placeholder">
-                        <h2>物件情報</h2>
-                    </div>
-                    :
-                    <div className="preview-bukken_info">
-                        <div className="preview-bukken_info_left">
-                        <p>面積: <b>{value.area} ㎡</b></p>
-                        </div>
-                        {value.published && (
-                            <p>公開日: <b>{value.published}</b></p>
-                        )}
-                        <div className="preview-bukken_info_right">
-                        </div>
-                    </div>
                     }
                 </div>
             </div>
